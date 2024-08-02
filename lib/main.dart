7f48a59a3dda99acf3448fd0d8e6393647ed5701
Williamparededl1 <wparedesl1995@gmail.com>
2024-08-01 23:37:14 -0500

    Creacion de la  Aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Pushes App'),
+          child: Text('Pushes App notificacion'),
         ),
       ),
     );
