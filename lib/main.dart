64110286072c415c24b04ea24de5b3e8ab402ae7
Williamparededl1 <wparedesl1995@gmail.com>
2024-08-02 00:00:18 -0500

    confg del Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:push_app/theme/app_theme.dart';
+import 'package:push_app/config/router/app_router.dart';
+import 'package:push_app/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,14 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       theme: AppTheme().getTheme(),
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Pushes App notificacion'),
-        ),
-      ),
     );
   }
 }
